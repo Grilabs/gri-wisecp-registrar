77741511ed9482817258f9d194963e502a30c932
fatihsari <fatih@fatihsari.com>
2023-08-08 10:21:46 +0300

    start github repository

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'meta' => [
+        'name' => 'Gri',
+        'version' => '1.0',
+        'logo' => 'logo.svg',
+    ],
+    'settings' => [
+        // whois-types : If it is desired to indicate that (registrant, administrative, technical, billing) communication types are supported in the module, "true" must be defined.
+        'whois-types' => true,
+        'username' => '',
+        'password' => '',
+        'api_client' => '',
+        'api_secret' => '',
+        'test-mode' => 0,
+        'adp' => false,
+        'cost-currency' => 4,
+    ]
+];
